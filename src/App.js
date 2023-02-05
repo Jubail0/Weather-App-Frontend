e93d0e5c7d6a142cd9f9cfe2238c9c84aa7ece9f
Jubail Mallick <98705208+Jubail0@users.noreply.github.com>
2023-02-05 15:24:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ function App() {
 
   React.useEffect(()=>{
     fetchWeatherData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   },[currentPage])
 
   const convertToCelcius = (kelvin)=>{
